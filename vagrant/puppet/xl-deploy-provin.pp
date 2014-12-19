5a7ac74e64dd2aa25ecdffac656861fa6bfad05b
Sedat Guclu <sguclu@finaxys.com>
2014-12-19 23:20:31 +0100

    Update xl-deploy-provin.pp
    XLD 4.0.0 module update

--- vagrant/puppet/xl-deploy-provin.pp
+++ vagrant/puppet/xl-deploy-provin.pp
@@ -1,14 +1,7 @@
 # XL DEPLOY INFRA/DICTIONARY/ENVIRONMENT/APPLICATION PROVISIONING THROUGH PUPPET MODULE ON A PROVISIONED UNIX+TOMCAT7 VM 
 
-# XL DEPLOY SERVER SETTINGS
-deployit { 'deployit-pipeline-demo': 
-  username => pipelinedemo_dev, 
-  password => pipelinedemo_dev, 
-  url => $url_deployit
-} 
-
 # INFRA SETTINGS
-deployit_container { $dev_cloud_host_id :
+xldeploy_ci { $dev_cloud_host_id :
   ensure => present, 
   type => 'overthere.SshHost', 
   properties => 
@@ -17,69 +10,72 @@ deployit_container { $dev_cloud_host_id :
 	  username => $dev_cloud_host_username, 
 	  password => $dev_cloud_host_password, 
 	  connectionType => 'SUDO', 
-	  sudoUsername => $dev_cloud_host_sudousername},
-   server => Deployit['deployit-pipeline-demo'], 
+	  sudoUsername => $dev_cloud_host_sudousername
+     },
+  rest_url => $dev_cloud_rest_url
 } 
 
-deployit_container { $dev_cloud_tomcat_id : 
+xldeploy_ci { $dev_cloud_tomcat_id : 
   ensure => present,
   type => 'tomcat.Server',
   require => Deployit_container[$dev_cloud_host_id],
   properties => { 
     startCommand => $dev_cloud_tomcat_startcommand
-	stopCommand => $dev_cloud_tomcat_stopcommand
-	home => $dev_cloud_tomcat_home 
-  server => Deployit['deployit-pipeline-demo'], 
-} 
+    stopCommand => $dev_cloud_tomcat_stopcommand
+    home => $dev_cloud_tomcat_home 
+  },
+rest_url => $dev_cloud_rest_url
+}
 
-deployit_container { $dev_cloud_tomcat_virtualhost_id : 
+xldeploy_ci { $dev_cloud_tomcat_virtualhost_id : 
   type => 'tomcat.VirtualHost', 
-  require => Deployit_container[$dev_cloud_tomcat_id], 
-  server => Deployit['deployit-pipeline-demo'], 
+  require => Deployit_container[$dev_cloud_tomcat_id],
+  tags => 'WEBAPP',
+  rest_url => $dev_cloud_rest_url 
 }
  
-deployit_container { $dev_cloud_tomcat_http_test_env_id :
+xldeploy_ci { $dev_cloud_tomcat_http_test_env_id :
   type => 'tests2.TestRunner', 
   require => Deployit_container[ $dev_cloud_host_id ] ,
   properties => {
     deploymentGroup => 2
   }, 
-  # TBC : HOW TO INCLUDE TAGS?
-  server => Deployit['deployit-pipeline-demo'] 
-  } 
+  tags => 'TEST-ENV',
+  rest_url => $dev_cloud_rest_url 
+} 
   
-deployit_container { $dev_cloud_tomcat_http_test_version_id :
+xldeploy_ci { $dev_cloud_tomcat_http_test_version_id :
   type => 'tests2.TestRunner', 
   require => Deployit_container[ $dev_cloud_host_id ] ,
   properties => {
     deploymentGroup => 3
   },
-  # TBC : HOW TO INCLUDE TAGS?
-  server => Deployit['deployit-pipeline-demo'] 
-  }
+  tags => 'TEST-VERSION',
+  rest_url => $dev_cloud_rest_url 
+}
   
 # DICTIONARIES SETTINGS  
-deployit_ dictionary { $dev_cloud_dictionary_id : 
+xldeploy_ci { $dev_cloud_dictionary_id : 
   before => $dev_cloud_env_id,   
   entries => {
     'test.url' => $dev_cloud_test_url_id,
     'environment' => $dev_cloud_environment
-    }
-  server => Deployit['deployit-pipeline-demo'] 
+    },
+  rest_url => $dev_cloud_rest_url
 }
 
 # ENVIRONMENT SETTINGS
-deployit_environment { $dev_cloud_env_id :
+xldeploy_ci { $dev_cloud_env_id :
   ensure => present,
-  containers => [ $dev_cloud_host_id,$dev_cloud_tomcat_id,$dev_cloud_tomcat_virtualhost_id,$dev_cloud_tomcat_http_test_env_id,$dev_cloud_tomcat_http_test_version_id]
-   dictionaries => [ $dev_cloud_dictionary_id ]
-  server => Deployit['deployit-pipeline-demo'] 
-  }
+  containers => [ $dev_cloud_host_id,$dev_cloud_tomcat_id,$dev_cloud_tomcat_virtualhost_id,$dev_cloud_tomcat_http_test_env_id,$dev_cloud_tomcat_http_test_version_id],
+  dictionaries => [ $dev_cloud_dictionary_id ],
+  rest_url => $dev_cloud_rest_url 
+}
   
 # APPLICATION SETTINGS
-deployed_application { $dev_cloud_deployed_application : 
+xldeploy_ci { $dev_cloud_deployed_application : 
   version => $dev_cloud_deployable_id,
   environment => $dev_cloud_env_id,
-  require => Deployit_container [ $dev_cloud_tomcat_virtualhost_id,$dev_cloud_tomcat_http_test_env_id,$dev_cloud_tomcat_http_test_version_id ]
-  server => Deployit['deployit-pipeline-demo'] 
+  require => Deployit_container [ $dev_cloud_tomcat_virtualhost_id,$dev_cloud_tomcat_http_test_env_id,$dev_cloud_tomcat_http_test_version_id ],
+  rest_url => $dev_cloud_rest_url 
 } 
