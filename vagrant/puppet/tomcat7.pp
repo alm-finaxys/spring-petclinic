0abc990e5d7f8a9f6478b8411299b7ff33c5bd12
alm-finaxys <sedat.guclu@finaxys.com>
2015-04-09 21:52:17 +0200

    Update tomcat7.pp

--- vagrant/puppet/tomcat7.pp
+++ vagrant/puppet/tomcat7.pp
@@ -57,6 +57,7 @@ file { '/var/lib/tomcat7/lib':
 
 # S. GUCLU : tweak used to disable authentication issues on sudo-managed deployments
 exec { 'sudo all for vagrant user':
-   command => 'sudo "echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/vagrant"',
+   environment => SUDOCOMMAND="vagrant ALL=(ALL) NOPASSWD:ALL",
+   command => 'sudo "echo $SUDOCOMMAND >> /etc/sudoers.d/vagrant"',
    require => Service["tomcat7"],
 }
